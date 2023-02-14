ac25d12a38d55017437688c127d165dc67da3dc4
lina drechsler <87331786+lina-drechsler@users.noreply.github.com>
2023-02-14 09:32:36 -0600

    sidebar first draft and forum page skeleton

--- frontend/pages/forum-page.jsx
+++ frontend/pages/forum-page.jsx
@@ -1,7 +1,15 @@
+import SideBar from "../components/all-pages/sidebar";
+
 export default function ForumPage() {
   return (
-    <div id="forum-page">
-      <h1>Forum Page</h1>
-    </div>
+    <div id="forum-page" className="flex h-screen w-screen grid-cols-2" >
+        <div className="w-[15%] "><SideBar/></div>
+        <div className= "w-[85%]">
+          <div className="flex h-1/4 justify-center text-4xl bg-blue-600"> Profile Header </div>
+
+          <div className="flex h-3/4 justify-center text-4xl bg-white"> Profile Contents </div>
+        </div>  
+      </div>
+    
   );
 }
