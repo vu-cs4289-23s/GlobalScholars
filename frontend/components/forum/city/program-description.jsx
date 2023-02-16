b3a3b0972a082540001d43da047b30e819a800c4
Tatiana Ciprich-Migdal <tatianaciprich-migdal@Tatianas-MacBook-Pro-2.local>
2023-02-15 20:43:13 -0600

    for pull

---
+++ frontend/components/forum/city/program-description.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+import { useLocation, useParams, useNavigate } from "react-router-dom";
+
+const programDescription = () => {
+  const navigate = useNavigate();
+  return(
+    <grid-rows-5>
+
+      grid
+
+    </grid-rows-5>
+  )
+
+}
--- frontend/pages/forum-page.jsx
+++ frontend/pages/forum-page.jsx
@@ -1,6 +1,9 @@
 export default function ForumPage() {
   return (
     <div id="forum-page">
+     <div className="columns-1">
+     </div>
+      <row></row>
       <h1>Forum Page</h1>
     </div>
   );
