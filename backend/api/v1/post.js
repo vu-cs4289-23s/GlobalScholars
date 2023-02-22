0dd231460e93ceb4b609f32f35938cd7f0bc753a
Clare <cbgippo@gmail.com>
2023-02-22 10:23:15 -0600

    GET post endpoints

--- backend/api/v1/post.js
+++ backend/api/v1/post.js
@@ -1,7 +1,6 @@
 import { object, string, array, date } from "yup";
 
 const Post = (app) => {
-
   /**
    * Create a new post
    *
@@ -62,7 +61,141 @@ const Post = (app) => {
     }
   });
 
+  // GET Post by id specified in request params
+  app.get(`/api/v1/post/:id`, async (req, res) => {
+    let data;
+    try {
+      data = await app.models.Post.findById(req.params.id);
 
+      if (!data) {
+        res.status(404).send({ error: `unknown post: ${req.params.id}` });
+      } else {
+        // Can do more here to fetch extra information
+        // Fetch User by Post owner id
+        res.status(200).send(data);
+      }
+    } catch (err) {
+      console.log(`Post.get failure: ${err}`);
+      res.status(404).send({ error: `unknown post: ${req.params.id}` });
+    }
+  });
+
+  // GET all Posts by location specified in request params
+  app.get(`/api/v1/posts/:location`, async (req, res) => {
+    let data;
+    try {
+      data = await app.models.Post.find({ location: req.params.location });
+
+      if (!data) {
+        res.status(404).send({ error: `unknown posts for location: ${req.params.location}` });
+      } else {
+        // Can do more here to fetch extra information
+        // Fetch User by Post owner id
+        res.status(200).send(data);
+      }
+    } catch (err) {
+      console.log(`Post.get failure: ${err}`);
+      res.status(404).send({ error: `unknown posts for location: ${req.params.location}` });
+    }
+  });
+
+  // GET all Posts by location and tags specified in request params
+  app.get(`/api/v1/posts/:location:tags`, async (req, res) => {
+    let data;
+    try {
+      data = await app.models.Post.find({
+        location: req.params.location,
+        tags: req.params.tags,
+      });
+
+      if (!data) {
+        res.status(404).send({ error: `unknown posts for location and tags: ${req.params.location} & ${req.params.tags}` });
+      } else {
+        // Can do more here to fetch extra information
+        // Fetch User by Post owner id
+        res.status(200).send(data);
+      }
+    } catch (err) {
+      console.log(`Post.get failure: ${err}`);
+      res.status(404).send({ error: `unknown posts for location and tags: ${req.params.location} & ${req.params.tags}` });
+    }
+  });
+
+  // GET all Posts by program specified in request params
+  app.get(`/api/v1/posts/:program`, async (req, res) => {
+    let data;
+    try {
+      data = await app.models.Post.find({ program: req.params.program });
+
+      if (!data) {
+        res.status(404).send({ error: `unknown posts for location: ${req.params.program}` });
+      } else {
+        // Can do more here to fetch extra information
+        // Fetch User by Post owner id
+        res.status(200).send(data);
+      }
+    } catch (err) {
+      console.log(`Post.get failure: ${err}`);
+      res.status(404).send({ error: `unknown posts for location: ${req.params.program}` });
+    }
+  });
+
+  // GET all Posts by program and tags specified in request params
+  app.get(`/api/v1/posts/:program:tags`, async (req, res) => {
+    let data;
+    try {
+      data = await app.models.Post.find({
+        program: req.params.program,
+        tags: req.params.tags,
+      });
+
+      if (!data) {
+        res.status(404).send({ error: `unknown posts for location and tags: ${req.params.program} & ${req.params.tags}` });
+      } else {
+        // Can do more here to fetch extra information
+        // Fetch User by Post owner id
+        res.status(200).send(data);
+      }
+    } catch (err) {
+      console.log(`Post.get failure: ${err}`);
+      res.status(404).send({ error: `unknown posts for location and tags: ${req.params.program} & ${req.params.tags}` });
+    }
+  });
+
+  // GET all Posts by User username specified in request params
+  app.get(`/api/v1/posts/:user`, async (req, res) => {
+    // Fetch user
+    let user;
+    try {
+      user = await app.models.User.findOne({ username: req.params.user });
+
+      // User does not exist
+      if (!user) {
+        res.status(404).send({ error: `unknown user: ${req.params.user}` });
+      }
+      else {
+        // Fetch posts by user
+        let data;
+        try {
+          data = await app.models.Post.find({ owner: user._id });
+
+          // Posts don't exist
+          if (!data) {
+            res.status(404).send({ error: `unknown posts for user: ${req.params.user}` });
+          } else {
+            // Successful fetch of posts, send back to client
+            res.status(200).send(data);
+          }
+        } catch (err) {
+          console.log(`Post.get failure: ${err}`);
+          res.status(404).send({ error: `unknown posts for user: ${req.params.user}` });
+        }
+      }
+    } catch (err) {
+      console.log(`Post.get failure: ${err}`);
+      res.status(404).send({ error: `unknown user: ${req.params.user}` });
+    }
+  });
 };
 
 export default Post;
