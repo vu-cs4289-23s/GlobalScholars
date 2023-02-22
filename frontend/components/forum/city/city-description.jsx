25afaa4966e06c11ca72704b04b0be554b9f58d7
Tatiana Ciprich-Migdal <tatianaciprich-migdal@Tatianas-MacBook-Pro-2.local>
2023-02-21 21:19:36 -0600

    PR

--- frontend/components/forum/city/city-description.jsx
+++ frontend/components/forum/city/city-description.jsx
@@ -1,11 +1,12 @@
 import { useLocation, useParams, useNavigate } from "react-router-dom";
-import Tag from "../tag.jsx";
-import Rating from "../rating.jsx";
+import Tag from "../all-forums/tag.jsx";
+import Rating from "../all-forums/rating.jsx";
+import ProgramLink from "../all-forums/program-link.jsx";
 
 const CityDescription = () => {
   const navigate = useNavigate();
   return (
-    <div className="flex bg-gray-400 bg-opacity-50 mx-20 text-left pt-2 pb-6 px-4">
+    <div className="flex bg-gray-400 bg-opacity-50 mx-20 text-left pt-2 pb-6 px-4 rounded-lg">
       <grid-cols-1>
         <span className="text-[30px]">
           <span className="content-start row ">
@@ -20,7 +21,7 @@ const CityDescription = () => {
           also famous for attractions such as Tivoli Gardens, The Little Mermaid
           statue, and the historic district of Nyhavn.
         </p>
-        <p className="py-2 font-bold text-[24px]">Top Tags</p>
+        <p className="py-4 font-bold text-[24px]">Top Tags</p>
         <span className="flex justify-around">
           <Tag />
           <Tag />
@@ -28,12 +29,16 @@ const CityDescription = () => {
           <Tag />
           <Tag />
         </span>
-        <p className="pt-2 pb-4 font-bold text-[24px]">Ratings</p>
+        <p className="py-4 font-bold text-[24px]">Ratings</p>
         <span className="flex justify-around">
           <Rating/>
           <Rating/>
           <Rating/>
           <Rating/>
+        </span>
+        <p className="py-4 font-bold text-[24px]">Like what you see? Study Here!</p>
+        <span>
+          {/*<ProgramLink/>*/}
         </span>
       </grid-cols-1>
     </div>
