38be6e8470760a06cf07710801a717733987af03
Clare <cbgippo@gmail.com>
2023-02-26 11:46:25 -0600

    cleanup'

--- frontend/components/forum/city/city-description.jsx
+++ frontend/components/forum/city/city-description.jsx
@@ -9,10 +9,6 @@ const CityDescription = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  // useEffect(() => {
-  //   dispatch()
-  // })
-
   return (
     <div className="grid grid-cols-3 sm:grid-cols-1 bg-gray-400 bg-opacity-50 mx-20 text-left pt-2 pb-6 px-4 rounded-lg absolute">
       <grid-cols-1>
