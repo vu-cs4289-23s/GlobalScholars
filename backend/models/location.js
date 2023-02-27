2b579a220e115d0f0b35068b1add8086e9ba639d
Clare <cbgippo@gmail.com>
2023-02-26 20:52:43 -0600

    updated models for rithik

--- backend/models/location.js
+++ backend/models/location.js
@@ -1,11 +1,16 @@
 import { Schema, model } from "mongoose";
 
 const Location = new Schema({
-  city: { type: String },
+  // Scraped data
+  city: { type: String, index: { unique: true }, required: true},
   country: { type: String },
   description: { type: String },
   programs: [ { type: Schema.ObjectId, ref: "Program" } ],
   like_cnt: { type: Number, default: 0 },
+
+  // Forum data
+  star_rating: { type: Number, default: 0 },
+  top_tags: [{ type: String }],
 });
 
 export default model("Location", Location);
