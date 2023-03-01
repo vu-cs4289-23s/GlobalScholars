9eb7937550a762a0905ec02b1a8c817ccc531404
Tatiana Ciprich-Migdal <tatianaciprich-migdal@Tatianas-MacBook-Pro-2.local>
2023-02-28 19:01:03 -0600

    making city description more dynamic

--- frontend/components/forum/city/city-description.jsx
+++ frontend/components/forum/city/city-description.jsx
@@ -3,13 +3,21 @@ import Rating from "../all-forums/rating.jsx";
 import ProgramLink from "../all-forums/program-link.jsx";
 import Reviews from "../../profile-page/reviews";
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+// import { getForumDataByName }  from "../redux/geo/geo-slice.js";
+import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
 
 const CityDescription = ({ city, country, description }) => {
+
+  const dispatch = useDispatch();
+
   // Format data
   city = city.charAt(0).toUpperCase() + city.slice(1).toLowerCase();
   country = country.charAt(0).toUpperCase() + country.slice(1).toLowerCase();
   const [object, setObject] = useState({});
+  const { programInfo, locationInfo } = useSelector((state)  => state.geo);
+
 
   const getData = () => {
     axios
@@ -45,18 +53,18 @@ const CityDescription = ({ city, country, description }) => {
         </p>
         <p className="py-4 font-bold text-[24px]">Top Tags</p>
         <div className="grid grid-cols-3 sm:grid-cols-5 justify-around justify-items-center">
-          <Tag content={"Weekend trip"} />
-          <Tag content={"Very affordable"} />
-          <Tag content={"Walkable"} />
-          <Tag content={"Awesome nightlife"} />
-          <Tag content={"Amazing eats"} />
+          <Tag content={locationInfo.top_tags[0]} />
+          <Tag content={locationInfo.top_tags[1]} />
+          <Tag content={locationInfo.top_tags[2]} />
+          <Tag content={locationInfo.top_tags[3]} />
+          <Tag content={locationInfo.top_tags[4]} />
         </div>
         <p className="py-4 font-bold text-[24px]">Ratings</p>
         <div className="grid grid-cols-1 sm:grid-cols-4 justify-around justify-items-center text-center">
-          <Rating rating={4.2} type={"Overall"} />
-          <Rating rating={3.7} type={"Safety"} />
-          <Rating rating={4.4} type={"Affordability"} />
-          <Rating rating={2.9} type={"Sightseeing"} />
+          <Rating rating={locationInfo.star_rating} type={"Overall"} />
+          {/*<Rating rating={3.7} type={"Safety"} />*/}
+          {/*<Rating rating={4.4} type={"Affordability"} />*/}
+          {/*<Rating rating={2.9} type={"Sightseeing"} />*/}
         </div>
         <p className="py-4 font-bold text-[24px]">
           Like what you see? Study Here!
