95527349d83c13ce0372adeef613929aab06cd27
Davi-web <jeseung.ha@vanderbilt.edu>
2023-03-01 17:34:22 -0600

    updating reviews component

--- frontend/components/profile-page/reviews.jsx
+++ frontend/components/profile-page/reviews.jsx
@@ -1,3 +1,5 @@
+import { FaThumbsUp, FaThumbsDown } from "react-icons/fa";
+import { BsBookmark } from "react-icons/bs";
 const Reviews = ({
   key,
   id,
@@ -9,6 +11,7 @@ const Reviews = ({
   saves,
   tags,
   dislikes,
+  type,
   comments,
 }) => {
   return (
@@ -34,11 +37,14 @@ const Reviews = ({
           </p>
         </div>
         <div
-          className="flex flex-row bg-gray-200 w-[80%] h-[90%] rounded-md m-4"
+          className="flex flex-row w-[80%] h-[90%] rounded-md m-4"
           id="reviews container"
         >
           <div className="flex flex-col w-full h-full items-center justify-start">
-            <div className="flex flex-row w-[90%] h-[70%] text-left  bg-green-500 rounded relative top-2">
+            <div
+              className="flex flex-row w-[90%] h-[70%] text-left bg-gray-200 rounded relative top-2"
+              id="reviews content"
+            >
               <p className=" p-4">{content}</p>
             </div>
             <div className="flex flex-row w-[90%] h-auto text-left  rounded relative top-2">
@@ -46,8 +52,11 @@ const Reviews = ({
                 placeholder="Leave a comment"
                 className=" h-[50%] w-full flex justify-start relative top-4 rounded-md focus:scale-150 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent focus:ease-linear transition-all duration-300 ease-in-out "
               />
-              <p>Thumbs up</p>
-              <p>Thumbs down</p>
+              <div className="flex justify-center items-center px-2">
+                <FaThumbsUp height={80} width={80} className="h-6 w-6" />
+                <FaThumbsDown height={80} width={80} className="h-6 w-6 ml-2" />
+                <BsBookmark height={80} width={80} className="h-6 w-6 ml-2" />
+              </div>
             </div>
           </div>
         </div>
