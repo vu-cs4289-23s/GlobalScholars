61a2c31a7fc06882d87f7f5582b35e62d4b375bb
lina drechsler <87331786+lina-drechsler@users.noreply.github.com>
2023-03-02 00:22:09 -0600

    saving local changes

--- frontend/pages/landing-page.jsx
+++ frontend/pages/landing-page.jsx
@@ -22,60 +22,6 @@ export default function LandingPage() {
       dispatch(getUserAsyncAction(userInfo.username));
     }
   }, [loggedIn, userInfo]);
-<<<<<<< HEAD
-
-  //get all landing pages into images 
-  var images = [{name: "Barcelona, Spain",  src: "frontend/images/landing-locations/barcelona.jpg"},
-      {name: "Copenhagen, Denmark", src: "frontend/images/landing-locations/copenhagen.jpeg"}, 
-      {name: "Edinburgh, Scotland", src: "frontend/images/landing-locations/edinburgh.jpeg"},
-      {name: "Florence, Italy", src:"frontend/images/landing-locations/florence.jpeg"},
-      {name: "Glasgow, Scotland", src:"frontend/images/landing-locations/glasgow.jpeg"},
-      {name: "London, United Kingdom", src:"frontend/images/landing-locations/london.jpeg"},
-      {name: "Madrid, Spain", src:"frontend/images/landing-locations/madrid.jpeg"},
-      {name: "Paris, France", src:"frontend/images/landing-locations/paris.jpeg"},
-      {name: "Rome, Italy", src:"frontend/images/landing-locations/rome.jpeg"},
-      {name: "Seville, Spain", src:"frontend/images/landing-locations/seville.jpeg"},
-      {name: "Stockholm, Sweden", src:"frontend/images/landing-locations/stockholm.jpeg"},
-      {name: "Prague, Czech Republic", src:"frontend/images/landing-locations/prague.jpeg"},
-      {name: "Budapest, Hungary", src:"frontend/images/landing-locations/budapest.jpeg"},
-      {name: "Amsterdam, Netherlands", src:"frontend/images/landing-locations/Amsterdam, Netherlands.jpeg"},
-      {name: "Belfast, United Kingdom", src: "frontend/images/landing-locations/Belfast, United Kingdom.jpeg"},
-      {name: "Belgrade, Serbia", src:"frontend/images/landing-locations/Belgrade, Serbia.jpeg"},
-      {name: "Berlin, Germany", src:"frontend/images/landing-locations/Berlin, Germany.jpeg"},
-      {name: "Bremen, Germany", src:"frontend/images/landing-locations/Bremen, Germany.jpeg"},
-      {name: "Cork, Ireland", src:"frontend/images/landing-locations/Cork, Ireland.jpeg"},
-      {name: "Dublin, Ireland", src:"frontend/images/landing-locations/Dublin, Ireland.jpeg"},
-      {name: "Freiburg, Germany", src:"frontend/images/landing-locations/Freiburg, Germany.jpeg"},
-      {name: "Galway, Ireland", src:"frontend/images/landing-locations/Galway, Ireland.jpeg"},
-      {name: "Geneva, Switzerland", src:"frontend/images/landing-locations/Geneva, Switzerland.jpg"},
-      {name: "Granada, Spain", src:"frontend/images/landing-locations/Granada, Spain.jpeg"},
-      {name: "Haifa, Israel", src:"frontend/images/landing-locations/Haifa, Israel.jpeg"},   
-      {name: "Jerusalem, Israel", src:"frontend/images/landing-locations/Jerusalem, Israel.jpeg"},
-      {name: "Leeds, United Kingdom", src:"frontend/images/landing-locations/Leeds, United Kingdom.jpeg"},
-      {name: "Metz, France", src:"frontend/images/landing-locations/Metz, France.jpeg"},
-      {name: "Milano, Italy", src:"frontend/images/landing-locations/Milano, Italy.jpeg"},
-      {name: "Palma, Spain", src:"frontend/images/landing-locations/Palma, Spain.jpeg"},
-      {name: "Siena, Italy", src:"frontend/images/landing-locations/Siena, Italy.jpeg"},
-      {name: "St Andrews, United Kingdom", src:"frontend/images/landing-locations/St Andrews, United Kingdom.jpeg"},
-      {name: "Stirling, United Kingdom", src:"frontend/images/landing-locations/Stirling, United Kingdom.jpeg"},
-      {name: "Tel Aviv, Israel", src:"frontend/images/landing-locations/Tel Aviv, Israel.jpeg"},
-      {name: "Toulouse, France", src:"frontend/images/landing-locations/Toulouse, France.jpeg"},
-      {name: "Turin, Italy", src:"frontend/images/landing-locations/Turin, Italy.jpeg"},
-      {name: "York, United Kingdom", src:"frontend/images/landing-locations/York, United Kingdom.jpeg"},]
-      
-  const {locationInfo} = useSelector((state)  => state.geo);
-
-  const onImageClick = (event, name) => {
-    console.log(event);
-    console.log(name);
-    // setShowPrograms(name);
-    console.log(name.split(",")[0].toLowerCase())
-    dispatch(getLocationByNameAsyncAction(name.split(",")[0].toLowerCase()))
-
-    const programs = locationInfo.programs;
-  }
-=======
->>>>>>> 19fb04622f5205c14745409c1e446d0bb94e38e1
  
     //dynamic array to hold program images & names (will be populated on a click of a location)
     const [programImages, setProgramImages] = useState([]);
