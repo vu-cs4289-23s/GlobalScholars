9db85f4924a176eedb912cdfe8954a49120eb4d3
Clare <cbgippo@gmail.com>
2023-03-02 13:58:28 -0600

    forum touchups

--- frontend/pages/forum-page.jsx
+++ frontend/pages/forum-page.jsx
@@ -13,14 +13,45 @@ export default function ForumPage() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const { name } = useParams();
-  const [city, setCity] = useState("City");
-  const [country, setCountry] =  useState("Country");
-  const [description, setDescription] =  useState("This is my description");
-  const [tags, setTags] = useState(["Tag One", "Tag Two", "Tag Three", "Tag Four", "Tag Five"]);
-  const [overallRate, setOverallRate] = useState(0);
-  const [safetyRate, setSafetyRate] = useState(0);
-  const [affordableRate, setAffordableRate] = useState(0);
-  const [sightsRate, setSightsRate] = useState(0);
+  const [location, setLocation] = useState({
+    city:  "City",
+    country: "Country",
+    description: "This is my city description",
+    programs: [],
+    top_tags: [
+      "Tag One",
+      "Tag Two",
+      "Tag Three",
+      "Tag Four",
+      "Tag Five"
+    ],
+    overall_rating: 0,
+    safety_rating: 0,
+    affordability_rating: 0,
+    sightseeing_rating: 0,
+    image_link: "",   // TODO -- add location image links to DB
+    like_cnt: 0,
+  });
+
+  // TODO -- make forum dynamic for programs
+  const [program, setProgram] = useState({
+    program_name: "Program",
+    description: "This is my program description",
+    location: [],
+    top_tags: [
+      "Tag One",
+      "Tag Two",
+      "Tag Three",
+      "Tag Four",
+      "Tag Five"
+    ],
+    overall_rating: 0,
+    safety_rating: 0,
+    affordability_rating: 0,
+    sightseeing_rating: 0,
+    image_link: "",
+    like_cnt: 0,
+  });
 
   const logOutHandle = () => {
     dispatch(logoutAction());
@@ -47,16 +78,38 @@ export default function ForumPage() {
   useEffect(() => {
     // Set Location data
     if (locationInfo && locationInfo.city !== "") {
-      setCity(locationInfo.city);
-      setCountry(locationInfo.country);
-      setDescription(locationInfo.description);
-      setTags(locationInfo.top_tags);
-      setOverallRate(locationInfo.overall_rating);
-      setSafetyRate(locationInfo.safety_rating);
-      setAffordableRate(locationInfo.affordability_rating);
-      setSightsRate(locationInfo.sightseeing_rating);
+      setLocation({
+        city:  locationInfo.city,
+        country: locationInfo.country,
+        description: locationInfo.description,
+        top_tags: locationInfo.top_tags,
+        overall_rating: locationInfo.overall_rating,
+        safety_rating: locationInfo.safety_rating,
+        affordability_rating: locationInfo.affordability_rating,
+        sightseeing_rating: locationInfo.sightseeing_rating,
+        image_link: locationInfo.image_link,
+        like_cnt: locationInfo.like_cnt,
+      });
     }
   }, [locationInfo]);
+
+  useEffect(() => {
+    // Set Program data
+    if (programInfo && programInfo.program_name !== "") {
+      setProgram({
+        program_name: programInfo.program_name,
+        description: programInfo.description,
+        location: programInfo.location,
+        top_tags: programInfo.top_tags,
+        overall_rating: programInfo.overall_rating,
+        safety_rating: programInfo.safety_rating,
+        affordability_rating: programInfo.affordability_rating,
+        sightseeing_rating: programInfo.sightseeing_rating,
+        image_link: programInfo.image_link,
+        like_cnt: programInfo.like_cnt,
+      });
+    }
+  }, [programInfo]);
 
   return (
     <div id="forum-page" className="flex h-screen w-screen bg-blue-200">
@@ -70,14 +123,14 @@ export default function ForumPage() {
         />
         {/*src="../../frontend/assets/landing-page-locations/{locationInfo[0].city}.jpg"*/}
         <CityDescription
-          description={description}
-          city={city}
-          country={country}
-          top_tags={tags}
-          overall_rating={overallRate}
-          safety_rating={safetyRate}
-          affordability_rating={affordableRate}
-          sightseeing_rating={sightsRate}
+          description={location.description}
+          city={location.city}
+          country={location.country}
+          top_tags={location.top_tags}
+          overall_rating={location.overall_rating}
+          safety_rating={location.safety_rating}
+          affordability_rating={location.affordability_rating}
+          sightseeing_rating={location.sightseeing_rating}
         />
       </div>
       <div className="absolute right-1 top-2">
