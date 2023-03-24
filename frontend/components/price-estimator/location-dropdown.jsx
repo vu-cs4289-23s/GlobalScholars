e67b966dc19e1846ed206b0fe86ffb44c3d08637
Davi-web <jeseung.ha@vanderbilt.edu>
2023-03-23 23:46:53 -0500

    Added location dropdown bar

--- frontend/components/price-estimator/location-dropdown.jsx
+++ frontend/components/price-estimator/location-dropdown.jsx
@@ -20,7 +20,7 @@ export default function LocationDropDown() {
       <Menu as="div" className="fixed inline-block  text-left">
         <div>
           <Menu.Button className="inline-flex w-full justify-center rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 ">
-            Options
+            Locations
             <BsChevronDown
               className="ml-2 -mr-1 h-5 w-5 text-violet-200 hover:text-violet-100"
               aria-hidden="true"
