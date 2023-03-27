182a307e8ed3feec4e78d491c18d7a8ca09956d0
lina drechsler <87331786+lina-drechsler@users.noreply.github.com>
2023-03-27 13:57:39 -0500

    updates to the map- trying to get markers to work

--- frontend/components/price-estimator/map.jsx
+++ frontend/components/price-estimator/map.jsx
@@ -1,34 +1,80 @@
-import React, { useRef, useEffect, useState } from 'react';
+import { useEffect, useRef } from 'react';
 import maplibregl from 'maplibre-gl';
-import 'maplibre-gl/dist/maplibre-gl.css';
 
-
-export default function Map(){
+const Map = ({  }) => {
   const mapContainer = useRef(null);
-  const map = useRef(null);
-  const [lng] = useState(13.0550);
-  const [lat] = useState(47.8095);
-  const [zoom] = useState(4);
-  const [API_KEY] = useState('CqDXCNXAkUrEn4wMkI6B');
+  let map = undef;
+  const lng = 12;
+  const lat = 88;
+  const zoom = 3;
+  const markers = [
+    { lng: -77.0369, lat: 38.9072 }, // Washington, DC
+    { lng: -73.9352, lat: 40.7306 }, // New York, NY
+    { lng: -122.4194, lat: 37.7749 } // San Francisco, CA
+  ];
+  useEffect(() => {
+
+    // add markers to the map
+markers.forEach(marker => {
+  if (!map) return;
+    const el = document.createElement('div');
+    el.className = 'marker';
+    new maplibregl.Marker(el)
+      .setLngLat([marker.lng, marker.lat])
+      .addTo(map);
+  });
+  }, [map])
+
+
 
   useEffect(() => {
-    if (map.current) return;
-    map.current = new maplibregl.Map({
+    if (!mapContainer.current) return;
+
+    map = new maplibregl.Map({
       container: mapContainer.current,
-      style: `https://api.maptiler.com/maps/streets-v2/style.json?key=${API_KEY}`,
+      style: `https://api.maptiler.com/maps/streets-v2/style.json?key=CqDXCNXAkUrEn4wMkI6B`,
       center: [lng, lat],
       zoom: zoom
     });
 
-  });
+    map.on('style.load', function () {
+      // Add your code to add layers, markers, etc. here
+      map.addSource('markers', {
+        type: 'geojson',
+        data: {
+          type: 'FeatureCollection',
+          features: markers
+        }
+      });
 
+      map.addLayer({
+        id: 'markers',
+        type: 'symbol',
+        source: 'markers',
+        layout: {
+          'icon-image': 'custom-marker',
+          'icon-allow-overlap': true,
+          'icon-size': 0.5,
+          'text-field': '{title}',
+          'text-font': ['Open Sans Semibold', 'Arial Unicode MS Bold'],
+          'text-offset': [0, 0.6],
+          'text-anchor': 'top'
+        }
+      });
+    });
+  }, [mapContainer, lng, lat, zoom, markers]);
+
+  
   return (
-    
     <div class="relative w-full h-full" >
         <div ref={mapContainer}  class=" absolute w-3/4 h-full ">
         </div>
     </div>
-    
-       
   );
 }
+
+export default Map;
+
+
+  
+
