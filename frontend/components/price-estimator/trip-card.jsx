1cc81b6924983dc2b8d2f38b8a2c804483e392a3
lina drechsler <87331786+lina-drechsler@users.noreply.github.com>
2023-03-28 21:52:08 -0500

    added save icon with state in price estimator. fixed trip-card layout as well

--- frontend/components/price-estimator/trip-card.jsx
+++ frontend/components/price-estimator/trip-card.jsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { BiBookmarkPlus, BiBookmarkMinus } from 'react-icons/bi';
 
 const Card = ({ city1, city2, rating, price, cheapestDates, imageUrl }) => {
+  const [saved, setSaved] = useState(false);
+
+  const handleSaveClick = () => {
+    setSaved(!saved);
+  };
+
   return (
-    <div className="bg-white rounded-lg shadow-lg flex p-6 ml-4 mr-4 h-48">
+    <div className="bg-white rounded-lg shadow-lg flex p-6 ml-4 mr-4 w-2/3">
       <div
         className="h-full w-48 bg-cover bg-center rounded-lg mr-4"
         style={{ backgroundImage: `url(${imageUrl})` }}
@@ -12,7 +19,10 @@ const Card = ({ city1, city2, rating, price, cheapestDates, imageUrl }) => {
           <h2 className="text-lg font-bold">{`${city1} - ${city2}`}</h2>
         </div>
         <div className="flex items-center mb-2">
-          <div className="flex ml-10 justify-items-center text-2xl">
+          <div className="mr-2">
+            <span className="text-sm text-gray-600">Rating:</span>
+          </div>
+          <div className="flex  justify-items-center text-2xl">
             <span className="text-yellow-600 mr-1 text-5xl">
               {rating >= 1 ? '★' : '☆'}
             </span>
@@ -36,11 +46,23 @@ const Card = ({ city1, city2, rating, price, cheapestDates, imageUrl }) => {
         <div className="text-sm text-gray-600 mb-2">
           <p>{`Cheapest Month: ${cheapestDates}`}</p>
         </div>
-      </div>
+        
     </div>
-  );
+    <div className="flex justify-end items-end w-full">
+        {saved ? (
+          <BiBookmarkMinus
+            className="text-black text-3xl cursor-pointer align-bottom justify-end"
+            onClick={handleSaveClick}
+          />
+        ) : (
+          <BiBookmarkPlus
+            className="text-black text-3xl cursor-pointer align-baseline justify-end"
+            onClick={handleSaveClick}
+          />
+        )}
+      </div>
+  </div>
+);
 };
 
 export default Card;
-
-
