fc58a8244978bbc8a5ed7160f75f602666f76e2d
Davi-web <jeseung.ha@vanderbilt.edu>
2023-04-04 21:21:04 -0500

    a

--- frontend/pages/price-estimator.jsx
+++ frontend/pages/price-estimator.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { getUserAsyncAction, logoutAction } from '../redux/user/user-slice';
 import { useSelector, useDispatch } from 'react-redux';
 import SideBar from '../components/all-pages/sidebar';
@@ -16,8 +16,6 @@ export default function PriceEstimator() {
   const { userInfo, loggedIn, success } = useSelector((state) => state.user);
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const [from, setFrom] = useState('');
-  const [to, setTo] = useState('');
   const logOutHandle = () => {
     dispatch(logoutAction());
   };
@@ -35,9 +33,9 @@ export default function PriceEstimator() {
       <SideBar />
 
       {/* page contents */}
-      <div className="overflow-x-hidden w-screen">
+      <div class="overflow-x-hidden w-screen">
         {/* header bar */}
-        <div className=" grid-row grid-col flex w-screen h-1/8 font-bold font-mono text-4xl ml-4 mt-16 text-center  text-black">
+        <div class=" grid-row grid-col flex w-screen h-1/8 font-bold font-mono text-4xl ml-4 mt-16 text-center  text-black">
           Where would you like to travel?
         </div>
         <div class=" grid-row flex w-screen h-1/6 absolutefont-bold text-2xl font-mono p-4 text-black">
@@ -56,10 +54,10 @@ export default function PriceEstimator() {
           </div>
         </div>
         <div className="ml-40 w-4/5 h-4/5 ">
-          <MapContainer from={from} to={to} setFrom={setFrom} />
+          <MapContainer />
         </div>
 
-        <div className="grid-row flex w-screen overflow-y-scroll h-2/3 absolutefont-bold font-mono p-4 text-black">
+        <div class="grid-row flex w-screen overflow-y-scroll h-2/3 absolutefont-bold font-mono p-4 text-black">
           <TabbedFolder tabs={tabs} card={<Card />} />
         </div>
 
@@ -71,3 +69,50 @@ export default function PriceEstimator() {
   );
 }
 
+// <div class="grid grid-cols-4 gap-4">
+// <div class="col-span-2 bg-blue-200 rounded-md p-4">
+// from
+// </div>
+
+// <div class="col-span-2 bg-green-200 rounded-md p-4">
+// <input type="text" class="w-full" placeholder="Start" />
+// </div>
+
+// <div class="col-span-2 bg-blue-200 rounded-md p-4">
+// to
+// </div>
+// <div class="col-span-2 bg-green-200 rounded-md p-4">
+// <input type="text" class="w-full" placeholder="End" />
+// </div>
+// </div>
+
+{
+  /* <div className="w-full overflow-y-scroll">
+        <div id="price-estimator-header" className="flex h-1/3 ml-5 text-4xl">
+          <div className="font-bold font-mono p-5 text-black mb-4">Where would you like to travel?</div>
+        </div>
+      </div> */
+}
+
+//   {/* destination/dates row */}
+//   <div class="flex col-span-3 h-1/4 grid grid-cols-3 gap-4">
+//   <div class="col-span-1 bg-gray-200 p-4">
+//     location dropdowns
+//   </div>
+//   <div class="col-span-1 bg-gray-300 p-4">
+//     date selectors
+//   </div>
+//   <div class="col-span-1 bg-gray-400 p-4">
+//     search icon
+//   </div>
+// </div>
+
+// {/* map element */}
+// <div class="col-span-1  w-screen bg-gray-200 p-4">
+//   Row 3, Column 1 Row 3, Column 1 Row 3, Column 1 Row 3, Column 1 Row 3, Column 1 Row 3, Column 1 Row 3, Column 1 Row 3, Column 1
+// </div>
+
+// {/* trip folder */}
+// <div class="col-span-1  w-screen bg-gray-300 p-4">
+//   Row 4, Column 1
+// </div>
