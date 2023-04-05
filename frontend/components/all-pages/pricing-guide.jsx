9ad0c83b68589f248c0991d90a06bcfc60693938
lina drechsler <87331786+lina-drechsler@users.noreply.github.com>
2023-04-05 13:16:57 -0500

    changed post for location

---
+++ frontend/components/all-pages/pricing-guide.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import { BsInfoCircleFill } from 'react-icons/bs';
+
+const AffordabilityInfoIcon = () => {
+  const [showPopup, setShowPopup] = useState(false);
+
+  return (
+    <div className="relative inline-block">
+      <BsInfoCircleFill
+        className="text-blue-500 hover:text-blue-800 cursor-pointer"
+        size={24}
+        onMouseEnter={() => setShowPopup(true)}
+        onMouseLeave={() => setShowPopup(false)}
+      />
+      {showPopup && (
+        <div className="absolute z-10 top-0 left-full ml-2 mt-1 p-2 bg-white border rounded shadow-md">
+          <p className="text-sm">
+            $: $100-$250<br />
+            $$: $250-$400<br />
+            $$$: $400+
+          </p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default AffordabilityInfoIcon;
