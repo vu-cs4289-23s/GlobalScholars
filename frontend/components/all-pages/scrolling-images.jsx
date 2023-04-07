2e6f476a5f2651a2e4c35b21a8ab9592702b324b
Davi-web <jeseung.ha@vanderbilt.edu>
2023-04-07 12:15:41 -0500

    added price estimator link and changed link url

--- frontend/components/all-pages/scrolling-images.jsx
+++ frontend/components/all-pages/scrolling-images.jsx
@@ -1,35 +1,48 @@
 import React from 'react';
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { getLocationByNameAsyncAction, getProgramByIdAsyncAction }  from "../../redux/geo/geo-slice.js";
-import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import {
+  getLocationByNameAsyncAction,
+  getProgramByIdAsyncAction,
+} from '../../redux/geo/geo-slice.js';
+import { useDispatch, useSelector } from 'react-redux';
 
-const ScrollingImages = ({images, url, rounded, forum}) => {
-  const [shape, setShape] = useState("h-52 w-52 object-cover border-4 border-white inline-block mx-3 transform transition hover:scale-125 hover:outline");
+const ScrollingImages = ({ images, url, rounded, forum }) => {
+  const [shape, setShape] = useState(
+    'h-52 w-52 object-cover border-4 border-white inline-block mx-3 transform transition hover:scale-125 hover:outline'
+  );
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  let { locationInfo } = useSelector((state)  => state.geo);
+  let { locationInfo } = useSelector((state) => state.geo);
   let [programs, setPrograms] = useState([]);
 
   useEffect(() => {
     if (rounded) {
-      setShape("h-52 w-52 rounded-full object-cover border-4 border-white inline-block mx-3 transform transition hover:scale-125 hover:outline");
+      setShape(
+        'h-52 w-52 rounded-full object-cover border-4 border-white inline-block mx-3 transform transition hover:scale-125 hover:outline'
+      );
     }
-  },[]);
+  }, []);
 
   const onClick = (ev) => {
     console.log(`Clicking on ${ev.target.name}`);
     // Navigate to forum page for location / program clicked
-    if(!forum){
-      navigate(`/forum/${ev.target.name.split(",")[0].toLowerCase()}`);
+    if (!forum) {
+      if (rounded) {
+        navigate(`/city/${ev.target.name.split(',')[0].toLowerCase()}`);
+      } else {
+        navigate(`/program/${ev.target.name.split(',')[0].toLowerCase()}`);
+      }
     }
   };
 
-  const onHover =  (ev) => {
+  const onHover = (ev) => {
     console.log(`Hovering over ${ev.target.name}`);
     if (rounded) {
       // Call  to API to fetch all programs under : ev.target.name.split(",")[0].toLowerCase()
-      dispatch(getLocationByNameAsyncAction(ev.target.name.split(",")[0].toLowerCase()));
+      dispatch(
+        getLocationByNameAsyncAction(ev.target.name.split(',')[0].toLowerCase())
+      );
     }
   };
 
@@ -41,43 +54,43 @@ const ScrollingImages = ({images, url, rounded, forum}) => {
     }
   }, [locationInfo]);
 
-    //params: images is the array to be passed in (with src photo and name)
-    //        shape is the shape in which the photo will be displayed (rounded or square)
-return (
+  //params: images is the array to be passed in (with src photo and name)
+  //        shape is the shape in which the photo will be displayed (rounded or square)
+  return (
     <div style={{ display: 'flex' }}>
-      { rounded ? images.map((image) => (
-          <div className="scroll-snap-align-start h-64 w-64">
-            <img 
+      {rounded
+        ? images.map((image) => (
+            <div className="scroll-snap-align-start h-64 w-64">
+              <img
                 src={image.src}
                 name={image.name}
-                alt={image.name} 
+                alt={image.name}
                 className={shape}
                 onClick={onClick}
                 onMouseOver={onHover}
-                />
-            <p className="text-base font-bold p-6 text-gray-900">
-            {image.name}
-            </p>
-         </div>
-      )) :
-        programs.map((program) => (
-          <div className="scroll-snap-align-start h-64 w-64">
-            <img
-              src={program.image_link}
-              name={program.program_name}
-              alt={program.program_name}
-              className={shape}
-              onClick={onClick}
-              onMouseOver={onHover}
-            />
-            <p className="text-base font-bold p-6 text-gray-900">
-              {program.program_name}
-            </p>
-          </div>))
-      }
+              />
+              <p className="text-base font-bold p-6 text-gray-900">
+                {image.name}
+              </p>
+            </div>
+          ))
+        : programs.map((program) => (
+            <div className="scroll-snap-align-start h-64 w-64">
+              <img
+                src={program.image_link}
+                name={program.program_name}
+                alt={program.program_name}
+                className={shape}
+                onClick={onClick}
+                onMouseOver={onHover}
+              />
+              <p className="text-base font-bold p-6 text-gray-900">
+                {program.program_name}
+              </p>
+            </div>
+          ))}
     </div>
   );
-
-}
+};
 
 export default ScrollingImages;
--- frontend/pages/price-estimator.jsx
+++ frontend/pages/price-estimator.jsx
@@ -1,4 +1,4 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { getUserAsyncAction, logoutAction } from '../redux/user/user-slice';
 import { useSelector, useDispatch } from 'react-redux';
@@ -15,8 +15,10 @@ const tabs = [{ title: 'My Trips' }, { title: 'Explore' }];
 
 export default function PriceEstimator() {
   const { userInfo, loggedIn, success } = useSelector((state) => state.user);
+  const { locationInfo, loading } = useSelector((state) => state.geo);
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const { city } = useParams();
   const logOutHandle = () => {
     dispatch(logoutAction());
   };
@@ -30,16 +32,12 @@ export default function PriceEstimator() {
     }
   }, [loggedIn, userInfo]);
   const [from, setFrom] = useState({
-    longitude: -122.4194,
-    latitude: 37.7749,
-    city: 'San Francisco',
+    city: 'Nashville',
+
+    longitude: -86.7816,
+    latitude: 36.1627,
   });
-  const [to, setTo] = useState({
-    longitude: -122.4194,
-    latitude: 37.7749,
-    city: 'San Francisco',
-  });
-  
+
   const [start, setStart] = useState(new Date());
   const [end, setEnd] = useState(new Date());
   const Search = (arg) => {
@@ -52,38 +50,51 @@ export default function PriceEstimator() {
     );
   };
 
-
+  useEffect(() => {
+    if (city !== undefined) {
+      //set from euqal to the longitude and latitude cooridnates from locqationInfo object
+      for (let i = 0; i < locationInfo.length; i++) {
+        if (locationInfo[i].city === city) {
+          setFrom({
+            city: locationInfo[i].city,
+            longitude: locationInfo[i].longitude,
+            latitude: locationInfo[i].latitude,
+          });
+        }
+      }
+    }
+  }, [city, locationInfo]);
 
   return (
     <div id="price-estimator" className="flex h-screen w-screen">
       <SideBar />
 
       {/* page contents */}
-      <div class="overflow-x-hidden w-screen">
+      <div className="overflow-x-hidden w-screen">
         {/* header bar */}
-        <div class=" grid-row grid-col flex w-screen h-1/8 font-bold font-mono text-4xl ml-4 mt-16 text-center  text-black">
+        <div className=" grid-row grid-col flex w-screen h-1/8 font-bold font-mono text-4xl ml-4 mt-16 text-center  text-black">
           Where would you like to travel?
         </div>
-        <div class=" grid-row flex w-screen h-1/6 absolutefont-bold text-2xl font-mono p-4 text-black">
-          <div class=" grid-col flex w-1/4 p-8 ml-6 z-30 text-black">
+        <div className=" grid-row flex w-screen h-1/6 absolutefont-bold text-2xl font-mono p-4 text-black">
+          <div className=" grid-col flex w-1/4 p-8 ml-6 z-30 text-black">
             DESTINATION:
             <LocationDropDown selected={from} setSelected={setFrom} />
           </div>
-          <div class=" grid-col  flex p-8 text-black">
+          <div className=" grid-col  flex p-8 text-black">
             START:
             <DateSelector selected={start} setSelectedDate={setStart} />
             END:
             <DateSelector selected={end} setSelectedDate={setEnd} />
           </div>
-          <div class=" grid-col flex p-8 ml-6 text-black">
+          <div className=" grid-col flex p-8 ml-6 text-black cursor-pointer">
             <ImSearch size={36} onClick={Search} />
           </div>
         </div>
         <div className="ml-28 w-4/5 h-4/5 ">
-          <MapContainer from={from} to={to} setFrom={setFrom} />
+          <MapContainer destination={from} setDestination={setFrom} />
         </div>
 
-        <div class="grid-row flex w-full overflow-y-scroll h-2/3 absolutefont-bold font-mono p-4 text-black">
+        <div className="grid-row flex w-full overflow-y-scroll h-2/3 absolutefont-bold font-mono p-4 text-black">
           <TabbedFolder tabs={tabs} card={<Card />} />
         </div>
 
@@ -94,4 +105,3 @@ export default function PriceEstimator() {
     </div>
   );
 }
-
