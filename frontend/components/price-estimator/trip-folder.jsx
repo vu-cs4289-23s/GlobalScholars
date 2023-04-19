fdcb4cc6b67f0ae88c59e2be7740bf564b874eed
lina drechsler <87331786+lina-drechsler@users.noreply.github.com>
2023-04-19 10:28:06 -0500

    deleted commented out code

--- frontend/components/price-estimator/trip-folder.jsx
+++ frontend/components/price-estimator/trip-folder.jsx
@@ -42,38 +42,7 @@ const TabbedFolder = ({ tabs, search, handleScrollToTop }) => {
         <MyTrips setTab={setActiveTab} />
       )}
     </div>
-    // <div className="flex flex-col h-full w-full mx-auto">
-    //   <div className="flex bg-sky-200  z-0">
-    //     {tabs.map((tab) => (
-    //       <div
-    //         key={tab.title}
-    //         className={` px-4 cursor-pointer rounded-md ${
-    //           activeTab === tab.title
-    //             ? 'bg-sky-200 font-semibold'
-    //             : 'bg-sky-100'
-    //         }`}
-    //         onClick={() => handleTabClick(tab.title)}
-    //       >
-    //         {tab.title}
-    //       </div>
-    //     ))}
-    //     <div class=" border-sky-200 -z-10  w-full transform rotate-[210deg] origin-bottom-right" />
-    //   </div>
-
-    //   <div
-    //     className={` bg-sky-300 h-full p-4 font-bold text-4xl flex flex-row justify-center z-20 shadow-2xl `}
-    //   >
-    //     <div className="justify-center align-middle ">
-    //       adding box shadow like apple folder
-    //       <div className="bg-sky-300 h-20 w-20 shadow-2xl"></div>
-    //     </div>
-    //     {activeTab === 'Explore' ? (
-    //       <Explore />
-    //     ) : (
-    //       <MyTrips setTab={setActiveTab} />
-    //     )}
-    //   </div>
-    // </div>
+   
   );
 };
 
