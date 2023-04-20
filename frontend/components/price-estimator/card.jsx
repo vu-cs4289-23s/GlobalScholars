605b74cd6f25d6f49f740409835cec38a69d694a
Davi-web <jeseung.ha@vanderbilt.edu>
2023-04-20 00:42:16 -0500

    added images from images.js

--- frontend/components/price-estimator/card.jsx
+++ frontend/components/price-estimator/card.jsx
@@ -43,8 +43,8 @@ const Card = ({
     <div className="flex flex-col rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 md:max-w-xl md:flex-row m-6">
       <img
         className="h-96 w-full rounded-t-lg object-cover md:h-auto md:w-32 md:rounded-none md:rounded-l-lg"
-        src="https://tecdn.b-cdn.net/wp-content/uploads/2020/06/vertical.jpg"
-        alt=""
+        src={image_link}
+        alt="/default_location.jpeg"
       />
       <div className="flex flex-col justify-start p-6 w-full h-full">
         <h5 className="w-full text-xl text-center font-medium text-neutral-800 dark:text-neutral-50">
